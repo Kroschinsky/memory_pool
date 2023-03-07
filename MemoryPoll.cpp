3d82f15241c9dd1fe1562dc4cb21dfc09d4b24d6
Theaso <jonatask@gmail.com>
2023-03-07 15:03:01 -0500

    start

---
+++ MemoryPoll.cpp
@@ -0,0 +1,12 @@
+﻿// MemoryPoll.cpp : Defines the entry point for the application.
+//
+
+#include "MemoryPoll.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
